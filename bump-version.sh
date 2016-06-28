06a8ae807b37e842335fab8b32e1c22846164318
Orfeo Da Via <orfeo.davia@microline.eu>
2016-06-28 15:27:11 +0200

    Added makefile

---
+++ bump-version.sh
@@ -0,0 +1,8 @@
+#!/bin/sh +v
+NEW_VER=$@
+
+sed -i -r "s/AssemblyVersion\(\"[0-9]+\.[0-9]+\.[0-9]+/AssemblyVersion\(\"${NEW_VER}/g" AssemblyInfo.cs
+sed -i -r "s/VERSION\s*=\s*\"[0-9]+\.[0-9]+\.[0-9]+/VERSION = \"${NEW_VER}/g" *.cs
+sed -i -r "s/VERSION\s*=\s*[0-9]+\.[0-9]+\.[0-9]+/VERSION = ${NEW_VER}/g" makefile
+make
+#git commit -a -m "Bumped version to ${NEW_VER}"
